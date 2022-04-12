241387d040d0f2194f4951bebe3c59685b89498a
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-12 17:07:58 -0300

    170712042022 - Project Create

---
+++ SistemaDeCadastro/Data/CadastroContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeCadastro.Models;
+
+namespace SistemaDeCadastro.Data
+{
+    public class CadastroContext : DbContext
+    {
+        public CadastroContext(DbContextOptions<CadastroContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
